b895180980481cf68fea168efcea89fa557a7555
Programming Hero <programming.hero1@gmail.com>
2022-10-11 22:18:10 -0500

    github initial setupt

--- __steps.js
+++ __steps.js
@@ -1,4 +1,7 @@
 /* 
+-----------------------------
+    INITIAL SETUP
+-----------------------------
 1. visit: console.firebase.google.com 
 2. create a new firebase project
 3. Visit doc ( go to docs): Build > Authentication > web > Getting started
@@ -6,6 +9,9 @@
 5. Install firebase for your project: npm install firebase
 6. DANGEROUS: get firebase config and put it in firebase.init.js
 7. export app from firebase.init.js 
+---------------------------
+    SETUP THE PROVIDER
+---------------------------
 8. create auth using getAuth from firebase by using app from firebase.init.js
 9. create a google auth provider. do not forget to use  new GoogleAuthProvider(); 
 10. go to firebase > Build > Authentication > Sign In method 
@@ -13,4 +19,15 @@
 12. Create a button for google sign in method with a click handler
 13. inside the event handler, call singInWithPopup with auth, provider
 14. after singInWithPopup .then result , error 
+-------------------------------
+    DISPLAY DATA
+-------------------------------
+
+--------------------------------
+    ADD A NEW SIGN IN METHOD
+-------------------------------
+1. enable the sign in  method
+2. Create github, twitter, fb, etc. app create
+3. get clientId and secret 
+
 */
--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
-    .then( result => {
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error => {
-      console.error('error: ', error)
-    })
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.error('error: ', error);
+      })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({})
+      })
   }
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sing In</button>
+      {/* condition ? true: false */}
+
+      {
+        user.email ?
+          <button onClick={handleSignOut}>Sign Out</button>
+          :
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sing In</button>
+            <button>Github Sign IN</button>
+          </>
+
+      }
+      {user.email && <div>
+        <h3>User name: {user.displayName}</h3>
+        <p>Email address: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
