9e105ccf58031da1b7770b445aee533c52561d21
Programming Hero <programming.hero1@gmail.com>
2022-10-11 20:42:43 -0500

    simple google sign in button

--- __steps.js
+++ __steps.js
@@ -6,4 +6,11 @@
 5. Install firebase for your project: npm install firebase
 6. DANGEROUS: get firebase config and put it in firebase.init.js
 7. export app from firebase.init.js 
+8. create auth using getAuth from firebase by using app from firebase.init.js
+9. create a google auth provider. do not forget to use  new GoogleAuthProvider(); 
+10. go to firebase > Build > Authentication > Sign In method 
+11. Enable google sign in method 
+12. Create a button for google sign in method with a click handler
+13. inside the event handler, call singInWithPopup with auth, provider
+14. after singInWithPopup .then result , error 
 */
--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import app from './firebase/firebase.init';
+
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then( result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.error('error: ', error)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sing In</button>
     </div>
   );
 }
